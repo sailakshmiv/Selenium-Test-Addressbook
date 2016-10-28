440581d5209e9eccba17f16fa047c8104ffd2aa6
goPaddle <testgopaddle@gmail.com>
2016-10-28 14:21:39 +0530

    Update Addressbook.java

--- src/org/bluemeric/com/Addressbook.java
+++ src/org/bluemeric/com/Addressbook.java
@@ -45,8 +45,8 @@ import junit.framework.Assert;
 	ExtentTest test = Suite.test;
 	Suite suite = new Suite();
 	WebDriver driver = suite.newDriver();
-	//static String url = "http://" + System.getProperty("APP_ENDPOINT");
-	static String url = "http://104.199.158.27:9006/";
+	static String url = "http://" + System.getProperty("APP_ENDPOINT");
+	//static String url = "http://104.199.158.27:9006/";
 	@Parameters({"suiteName"})
 	@BeforeMethod
 	 public void createreport(ITestContext arg0,@Optional String suiteName){
